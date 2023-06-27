4911f0f78d4a9e657da871fda0422eb840318d43
mohammadmac <m.m.mir83hc@gmail.com>
2023-06-27 11:44:57 +0330

    add ;

--- pacman/ghost.cpp
+++ pacman/ghost.cpp
@@ -13,8 +13,8 @@ void ghost::move(){
 int ghost::changeDirection(){
     int i = 0, j = 0, tempDirect;
     bool checkX = false, checkY = false;
-    float blocksX[] = {10.0, 74.0, 170.0, 252.0, 334.0, 418.0, 500.0, 584.0, 678.0, 740.0}
-    float blocksY[] = {8.0, 110.0, 194.0, 272.0, 342.0, 416.0, 492.0, 576.0, 660.0, 742.0}
+    float blocksX[] = {10.0, 74.0, 170.0, 252.0, 334.0, 418.0, 500.0, 584.0, 678.0, 740.0};
+    float blocksY[] = {8.0, 110.0, 194.0, 272.0, 342.0, 416.0, 492.0, 576.0, 660.0, 742.0};
     
     for (int k = 0; k < 10; k++){
         if (x() == blocksX[k]){
